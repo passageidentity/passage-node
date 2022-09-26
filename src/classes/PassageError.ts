4b63676541f65fecdf3e1b0eb30f46d8971598bd
Chris Loper <chrisloper@utexas.edu>
2022-09-26 13:15:01 -0500

    Change files

--- src/classes/PassageError.ts
+++ src/classes/PassageError.ts
@@ -4,22 +4,22 @@ import { AxiosError } from "axios";
  * Passage Class
  */
 export class PassageError extends Error {
-  readonly statusCode: number | undefined;
-  readonly statusText: string | undefined;
-  readonly error: string | undefined;
-  readonly message: string;
+    readonly statusCode: number | undefined;
+    readonly statusText: string | undefined;
+    readonly error: string | undefined;
+    readonly message: string;
 
-  /**
+    /**
    * Initialize a new PassageError instance.
    * @param {string} message friendly message,
    * @param {AxiosError} err error from axios request
    */
-  constructor(message: string, err?: AxiosError) {
-    super();
+    constructor(message: string, err?: AxiosError) {
+        super();
 
-    this.message = message;
-    this.statusCode = err?.response?.status;
-    this.statusText = err?.response?.statusText;
-    this.error = err?.response?.data.error;
-  }
+        this.message = message;
+        this.statusCode = err?.response?.status;
+        this.statusText = err?.response?.statusText;
+        this.error = err?.response?.data.error;
+    }
 }
