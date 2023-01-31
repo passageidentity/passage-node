9232e38d8188195e1ae0a41ff2660cf7a4f4fdc2
Chris Loper <chrisloper@utexas.edu>
2023-01-31 14:04:21 -0600

    JWT authentication tests imrpoved: (#49)
    - make sure to test HEADER auth JWT authentication
     - add more robust tests around invalid_tokens

--- tests/test-suite.test.ts
+++ tests/test-suite.test.ts
@@ -36,16 +36,37 @@ describe("Passage Initialization", () => {
 
   // note that the current token is only valid until Nov.8 2022
   test("authenticateRequestWithCookie", async () => {
-    await request(app).get("/").expect(401);
+    await request(app).get("/cookie").expect(401); // no token set --> 401
     await request(app)
-      .get("/")
+      .get("/cookie")
+      .set("Cookie", [`psg_auth_token=invalid_token`]) // invalid token set --> 401
+      .expect(401);
+    await request(app)
+      .get("/cookie")
       .set("Cookie", [`psg_auth_token=${appToken}`])
+      .expect(200);
+  });
+
+  test("authenticateRequestWithHeader", async () => {
+    await request(app).get("/header").expect(401); // no token set --> 401
+    await request(app)
+      .get("/header")
+      .set("Authorization", `Bearer invalid_token`)
+      .expect(401);
+    await request(app)
+      .get("/header")
+      .set("Authorization", `Bearer ${appToken}`)
       .expect(200);
   });
 
   test("validAuthToken", async () => {
     const userID = await passage.validAuthToken(appToken);
     expect(userID).toBe("bEXIZKYyApgz5oWYc5WM9vfF");
+  });
+
+  test("invalidAuthToken", async () => {
+    const userID = await passage.validAuthToken("invalid_token");
+    expect(userID).toBe(undefined);
   });
 });
 
