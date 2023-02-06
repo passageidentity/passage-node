aa83aa00630da0357ef2670614d72774e3f3bd4f
Kevin Flanagan <90063911+flanagankp@users.noreply.github.com>
2023-02-06 10:19:17 -0600

    Enforce email, phone, or user_id in the type system (#50)

--- src/types/MagicLink.ts
+++ src/types/MagicLink.ts
@@ -11,19 +11,28 @@ export type MagicLinkObject = {
   url: string;
 };
 
-enum ChannelEnum {
+export enum ChannelEnum {
   email = "email",
   phone = "phone",
 }
 export type ChannelType = keyof typeof ChannelEnum;
 
-export type MagicLinkRequest = {
+interface MagicLinkRequestProps {
   user_id: string;
   email: string;
   phone: string;
-  channel: ChannelEnum;
   send: boolean;
+  channel: ChannelType;
   magic_link_path: string;
   redirect_url: string;
   language: string;
-};
+  ttl: number;
+}
+
+/** MagicLinkRequest must contain at least one of an email, phone, or user_id property. Note, if you set a value for the send property you must also set a value for the channel.*/
+export type MagicLinkRequest = Partial<MagicLinkRequestProps> &
+  (
+    | Pick<MagicLinkRequestProps, "email">
+    | Pick<MagicLinkRequestProps, "phone">
+    | Pick<MagicLinkRequestProps, "user_id">
+  );
--- tests/test-suite.test.ts
+++ tests/test-suite.test.ts
@@ -81,8 +81,10 @@ describe("Passage API Requests", () => {
       const magicLink = await passage.createMagicLink({
         email: "chris@passage.id",
         channel: "email",
-      } as MagicLinkRequest);
+        ttl: 12,
+      });
       expect(magicLink).toHaveProperty("identifier", "chris@passage.id");
+      expect(magicLink).toHaveProperty("ttl", 12);
     });
 
     test("Get App", async () => {
