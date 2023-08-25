542f38696f4947d4dd8ba515553e95f51cfa2a0e
Vanessa Burroughs <vanessa.burroughs@passage.id>
2023-08-25 14:24:18 -0500

    Change files

--- scripts.sh
+++ scripts.sh
@@ -4,26 +4,26 @@
 case "$1" in
   "beachball-bump")
     echo "Running 'beachball bump'"
-    beachball bump
+    npx beachball bump
     ;;
   "beachball-check")
     echo "Running 'beachball check'"
-    beachball check
+    npx beachball check
     ;;
   "beachball-publish")
     echo "Running 'beachball publish -y -n'"
-    beachball publish -y -n
+    npx beachball publish -y -n
     ;;
   "beachball-change")
     echo "Running 'beachball change'"
-    beachball change
+    npx beachball change
     ;;
   "set-version")
     echo "{\"version\":\"$npm_package_name $npm_package_version\"}" > ./src/utils/config.json
     ;;
   "prepare")
     echo "Running 'husky install'"
-    husky install
+    npx husky install
     ;;
   *)
     echo "Invalid script name. Available options: beachball-bump, beachball-check, beachball-publish, beachball-change, set-version, prepare"
