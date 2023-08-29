2f5c2ae930727a030a1906b2e6f8fb5fd99a4e3e
Bert Ramirez <bert.ramirez@agilebits.com>
2023-08-29 11:29:06 -0500

    check in generated dir

---
+++ src/generated/apis/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AppsApi';
+export * from './MagicLinkApi';
+export * from './UserDevicesApi';
+export * from './UsersApi';
---
+++ src/generated/models/index.ts
@@ -0,0 +1,25 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AppInfo';
+export * from './AppResponse';
+export * from './CreateMagicLinkRequest';
+export * from './CreateUserRequest';
+export * from './ElementCustomization';
+export * from './FontFamily';
+export * from './LayoutConfig';
+export * from './Layouts';
+export * from './ListDevicesResponse';
+export * from './MagicLink';
+export * from './MagicLinkResponse';
+export * from './Model400Error';
+export * from './Model401Error';
+export * from './Model404Error';
+export * from './Model500Error';
+export * from './Technologies';
+export * from './UpdateUserRequest';
+export * from './UserEventInfo';
+export * from './UserInfo';
+export * from './UserMetadataField';
+export * from './UserMetadataFieldType';
+export * from './UserResponse';
+export * from './WebAuthnDevices';
