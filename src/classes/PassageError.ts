2ddced07214dabadc2951d908cc5824210ce1c83
Chris Tran <ctran88@users.noreply.github.com>
2024-10-17 13:14:18 -0500

    PSG-4065: fix PassageError error messages (#161)
    * fix: API error messages are now correctly mapped to the PassageError class
    
    * Change files

--- src/classes/PassageError.ts
+++ src/classes/PassageError.ts
@@ -4,24 +4,36 @@ import { ResponseError } from '../generated';
  * Passage Class
  */
 export class PassageError extends Error {
+    override name: 'PassageError' = 'PassageError';
     readonly statusCode: number | undefined;
     readonly error: string | undefined;
     readonly message: string;
 
     /**
      * Initialize a new PassageError instance.
-     * @param {string} message friendly message,
-     * @param {Error} err error from node-fetch request
+     * @param {string} message friendly message
+     * @param {ResponseError} err error from http request
      */
     constructor(message: string, err?: ResponseError) {
         super();
 
         if (err) {
-            this.message = `${message}: ${err.message}`;
+            this.message = message;
             this.statusCode = 500;
             this.error = err.message;
         } else {
             this.message = message;
         }
+    }
+
+    /**
+     * Maps a ResponseError to a PassageError.
+     * @param {error} err ResponseError
+     * @param {string} message ResponseError
+     * @return {PassageError} PassageError
+     */
+    public static async fromResponseError(err: ResponseError, message?: string): Promise<PassageError> {
+        const response: { code: string; error: string } = await err.response.json();
+        return new PassageError(`${message}: ${response.error}`, err);
     }
 }
