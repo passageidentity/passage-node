d0d42311b303a1d5555de86a5422177938704ab9
Kevin Flanagan <kevin.flanagan@passage.id>
2024-11-08 11:21:57 -0600

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,9 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+// jest.config.js
+const { createDefaultPreset } = require('ts-jest')
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    globals: {
-        'ts-jest': {
-            isolatedModules: true,
-        },
-    },
-};
+  transform: {
+    ...createDefaultPreset().transform,
+  },
+}
